ae0cf8f184b63bdcb69bd964f4a1950249ecdd79
Morgen Fett <mfett004@gmail.com>
2016-10-24 02:19:55 -0700

    registration things

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -15,5 +15,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^store/', include('store.urls'), name='store'),
+    url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
