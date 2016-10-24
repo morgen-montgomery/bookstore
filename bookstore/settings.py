8ede07c19d02350aa89230e791bb1f678f756a30
Morgen Fett <mfett004@gmail.com>
2016-10-24 01:57:27 -0700

    settings changed

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'registration',
     'store',
 ]
 
@@ -108,3 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Registration
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
