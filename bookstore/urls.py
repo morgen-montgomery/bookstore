2009b374677a726472be84d0cf9785e176ad59a5
Morgen Fett <mfett004@gmail.com>
2016-10-24 00:42:19 -0700

    template rendering

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,8 +1,4 @@
-"""bookstore URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
+"""
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
@@ -13,9 +9,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', 'store.views.index', name='index'),
+    url(r'^admin/', include(admin.site.urls)),
 ]
